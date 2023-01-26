db2746ffe6e658de007b75c4c32edefab197533e
Carlos Malky <akeemmalky@gmail.com>
2023-01-25 22:25:36 -0600

    backend JSON sending to BE

--- App.js
+++ App.js
@@ -1,20 +1,231 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  ImageBackground,
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import * as ImagePicker from "expo-image-picker";
+import React, { useState, useEffect } from "react";
+import { Video, AVPlaybackStatus } from "expo-av";
 
 export default function App() {
+  const [image, setImage] = useState(null);
+  const [video, setVideo] = useState(null);
+
+  const pickImageVideo = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    // console.log(result);
+
+    if (!result.cancelled) {
+      // setImage(result.uri);
+      // console.log("Result type: " + result.type);
+      if (result.type == "video") {
+        console.log("Result type: " + result.type);
+        console.log("uri: ", result.uri);
+        setVideo(result.uri);
+      } else {
+        console.log("Not video type");
+        setImage(result.uri);
+      }
+    }
+  };
+
+  async function sendToBackend() {
+    console.log("Uplaod pressed");
+    // FETCH API
+
+    const fetchResponse = await fetch("http://127.0.0.1:5000/addMedia", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        videoTitle: "Test Title",
+        videoDuration: "12:34",
+      }),
+    });
+
+    const jsonData = await fetchResponse.json();
+
+    console.log(jsonData);
+    console.log("holda dudelskjdflksjdfldskjd");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.topContainer}>
+        <ImageBackground
+          source={require("./assets/walking-background.png")}
+          resizeMode="cover"
+          style={styles.backgroundImage}
+        >
+          <Video
+            style={styles.tempVideo}
+            useNativeControls
+            resizeMode="contain"
+            isLooping
+            source={{
+              uri: video,
+            }}
+          />
+        </ImageBackground>
+      </View>
+      <View style={styles.bottomContainer}>
+        <LinearGradient
+          // Background Linear Gradient
+          colors={["rgba(155,168,213, 08)", "transparent"]}
+          style={styles.bottomGradient}
+        >
+          <Text style={styles.title}>Gait Identification</Text>
+          <Text style={styles.description}>
+            Please upload a video from gallery or record a new one.
+          </Text>
+
+          <TouchableOpacity
+            title="Upload from Gallery"
+            style={styles.buttonContainer}
+            onPress={pickImageVideo}
+          >
+            <Image
+              style={styles.buttonImg}
+              source={require("./assets/upload.png")}
+            ></Image>
+            <Text style={styles.buttonText}>Choose from Gallery</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            title="Upload from Gallery"
+            style={styles.buttonContainer}
+            onPress={sendToBackend}
+          >
+            <Image
+              style={styles.buttonImg}
+              source={require("./assets/upload.png")}
+            ></Image>
+            <Text style={styles.buttonText}>Upload</Text>
+          </TouchableOpacity>
+
+          {/* Demo to upload pictures/videos */}
+          {/* <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+          >
+            <Button
+              title="Pick an image from camera roll"
+              onPress={pickImage}
+            />
+            {image && (
+              <Image
+                source={{ uri: image }}
+                style={{ width: 200, height: 200 }}
+              />
+            )}
+          </View> */}
+          {/* Demo to upload pictures/videos */}
+
+          <TouchableOpacity
+            title="Record Video"
+            style={styles.buttonContainer}
+            onPress={() => Alert.alert("Open camera")}
+          >
+            <Image
+              style={styles.buttonImg}
+              source={require("./assets/camera.png")}
+            ></Image>
+            <Text style={styles.buttonText}>Record Video</Text>
+          </TouchableOpacity>
+        </LinearGradient>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    height: "100%",
+  },
+  topContainer: {
+    height: "40%",
+  },
+  bottomContainer: {
+    height: "60%",
+  },
+  bottomGradient: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    backgroundColor: "rgb(207,145,215)",
+    justifyContent: "start",
+    alignItems: "center",
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  tempVideo: {
+    alignSelf: "center",
+    width: "80%",
+    height: "80%",
+  },
+  title: {
+    color: "white",
+    fontSize: 38,
+    marginTop: "8%",
+  },
+  description: {
+    color: "#E6E6E6",
+    width: "55%",
+    margin: "6%",
+    fontSize: 18,
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "start",
+    backgroundColor: "rgba(255,255,255,0.45)",
+    paddingTop: "5%",
+    paddingBottom: "5%",
+    paddingLeft: "10%",
+    paddingRight: "10%",
+    borderRadius: "20",
+    width: "75%",
+    margin: "5%",
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "rgb(42,42,42)",
+    // backgroundColor: "red",
+    width: "100%",
+    textAlign: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
   },
 });
